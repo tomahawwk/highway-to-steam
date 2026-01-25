a07b53ae670a2d084e007dc5c0e3a9200f43cc9f
tomahawwk <t.ahawwk@gmail.com>
2026-01-26 00:16:25 +0300

    adds member

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,15 @@ const App = () => {
           HIGHWAY TO STEAM <span className="text-primary">2026</span>
         </div>
         <div className="text-gray-300">
-          Fund budget: <span className="text-white">60$</span>
+          Fund budget: <span className="text-white">80$</span>
         </div>
         <div className="flex flex-col gap-sm">
           <span className="text-white">Members:</span>
           <ul className="flex gap-2 justify-center">
             <li className="text-sm">tom_ahawk, </li>
             <li className="text-sm">aliery, </li>
-            <li className="text-sm">augenblick</li>
+            <li className="text-sm">augenblick, </li>
+            <li className="text-sm">plpdt</li>
           </ul>
         </div>
       </div>
